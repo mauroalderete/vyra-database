13a21a04b446fae7e631d25c24a5bcd7ef1614b9
Rayquen <rayquen.zero@gmail.com>
2020-12-27 01:22:21 -0300

    inicialiso tipos de comprobantes

--- inicializacion.sql
+++ inicializacion.sql
@@ -40,3 +40,20 @@ null,
 null,
 'Proveedor a usar cuando no es posible identificarlo');
 
+INSERT INTO `dbvyra`.`comp_comprobantes_tipos`
+(`CCTP_TIPO`,
+`CCTP_NOMBRE`,
+`CCTP_NOTAS`)
+VALUES
+('INI',
+'Ingresos iniciales',
+'Se usa para registrar los ingresos de los articulos al inicio de la actividad. Muchas veces el origen de estos articulos es desconocido, o no se refleja como un movimiento en la caja. Se recomienda dar de baja una vez que se haya configurado el inicio de la empresa');
+
+INSERT INTO `dbvyra`.`vent_comprobantes_tipos`
+(`CVTP_TIPO`,
+`CVTP_NOMBRE`,
+`CVTP_NOTAS`)
+VALUES
+('INI',
+'Egresos iniciales',
+'Se usa para registrar los egresos de articulos previos al inicio de la actividad. Muchas veces el origen o destino de los articulos es desconocido, pero se reflejan en el estado inicial de la empresa. Se recomienda dar de baja una vez que se haya configurado el inicio de la empresa');
